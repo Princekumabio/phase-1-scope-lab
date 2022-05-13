69e786139fbd1c160cd1da70e952556b0616225e
kumabio <kumabio.prince@student.moringaschool.com>
2022-05-13 21:11:37 +0000

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+const leastFavoriteCustomer = 'kumabio';
+
+function upperCaseCustomerName(){   
+    customerName.toUpperCase();
+}
+console.log(upperCaseCustomerName());
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'pedaiah';
+}
+
+console.log(changeLeastFavoriteCustomer());
